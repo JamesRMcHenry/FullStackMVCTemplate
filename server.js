749af87b17b31d3c747eaaa040f93fe3d6b4488d
JamesRMcHenry <100225606+JamesRMcHenry@users.noreply.github.com>
2023-02-01 14:38:50 -0500

    added css!

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set("view engine", "ejs");
 app.use(express.static('public'))
 
 //*Required to properly parse form POST requests - sending data
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 //todo - Set Routes
 app.use('/edit', editRoutes)
